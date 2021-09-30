956b3556d7d1ed148fd650fc7b989a5e56a5a40f
PisoPlays <91681778+PisoPlays@users.noreply.github.com>
2021-09-30 16:42:58 +0200

    Discord bot

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+
+
+client.once('ready', () => {
+    console.log('Ashima Discord Bot Is Online!')
+})
+
+
+
+
+
+client.login(process.env.token);
